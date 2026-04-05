31ec19004bb898580383f9f2ccaa427bb13eda6a
ixn3rd3mxn <ixn3rd3mxn@gmail.com>
2026-04-05 10:25:53 +0300

    alpha-1.1.7

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,4 @@
 import { provideTaiga } from '@taiga-ui/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -8,7 +7,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimations(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withFetch()),
